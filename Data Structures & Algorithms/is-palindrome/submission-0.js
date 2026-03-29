6afec8772ff59ce0c3f7f974721a1c200cd8e766
Carter Johnson <carter.bland2000@gmail.com>
2026-03-29 09:59:59 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            while (l < r && !/[a-z0-9]/i.test(s[l])) l++;
+
+            while (l < r && !/[a-z0-9]/i.test(s[r])) r--;
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
